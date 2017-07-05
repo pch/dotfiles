9c7a973353cd36fc2329e22d3124f5a8f52cc763
Piotr Chmolowski <piotr@chmolowski.pl>
2017-07-05 08:26:02 +0200

    Add welder to $PATH (see pch/welder)

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,2 +1,2 @@
-export PATH="./bin:$ZSH/bin:/usr/local/bin:/usr/local/sbin:$PATH"
+export PATH="./bin:$ZSH/bin:/usr/local/bin:/usr/local/sbin:$PATH:$HOME/Code/welder/bin"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"

1a103bb0e34e856c52a28c0e95e1f3707c56594e
Piotr Chmolowski <piotr@chmolowski.pl>
2021-11-06 13:22:05 +0100

    Update welder path

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,2 +1,2 @@
-export PATH="./bin:$ZSH/bin:/usr/local/bin:/usr/local/sbin:$PATH:$HOME/Code/welder/bin"
+export PATH="./bin:$ZSH/bin:/usr/local/bin:/usr/local/sbin:$PATH:$HOME/Code/welder"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"

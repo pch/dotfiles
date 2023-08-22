2ed466ca9cb8524dc49e430f9460df965e7a3041
pch <piotr@chmolowski.pl>
2023-08-22 13:17:19 +0000

    Upgrade docker-compose

--- ubuntu/system.sh
+++ ubuntu/system.sh
@@ -68,7 +68,7 @@ sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubun
 sudo apt-get update
 sudo apt-get install -y docker-ce
 sudo usermod -aG docker $USERNAME
-sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/v2.20.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
 # kubectl

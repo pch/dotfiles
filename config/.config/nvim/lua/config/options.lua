28b10715277fdf97fa759fe47ab0eb50b72d1fd9
Piotr Chmolowski <piotr@chmolowski.pl>
2025-11-07 11:14:34 +0100

    Remove lazyvim, set up from scratch

--- config/.config/nvim/lua/config/options.lua
+++ config/.config/nvim/lua/config/options.lua
@@ -1,3 +1,24 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
+vim.cmd [[colorscheme tokyonight-night]]
+
+vim.opt.clipboard = 'unnamedplus' -- use system keyboard for yank
+
+vim.opt.nu = true                 -- set line numbers -- set line numbers
+vim.opt.relativenumber = true     -- use relative line numbers
+
+-- set tab size to 2 spaces
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+
+vim.opt.incsearch = true -- incremental search
+
+vim.opt.termguicolors = true
+
+vim.opt.updatetime = 250 -- faster cursor hold events (for diagnostic popups)
+
+-- Show which line your cursor is on
+vim.o.cursorline = true

8d0d06775608b83abbc152cf390c7d022f8f5bdf
Piotr Chmolowski <piotr@chmolowski.pl>
2025-12-29 16:15:55 +0100

    nvim-treesitter seems to have been rewrittern (incompatible)

--- dotfiles/.config/nvim/lua/plugins/treesitter.lua
+++ dotfiles/.config/nvim/lua/plugins/treesitter.lua
@@ -1,9 +1,10 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  branch = "master",
   build = ":TSUpdate",
   config = function()
     local configs = require("nvim-treesitter.configs")
- 
+
     configs.setup({
       ensure_installed = {
         "c", "lua", "vim", "vimdoc", "go", "javascript", "html", "python", "typescript", "ruby", "markdown"

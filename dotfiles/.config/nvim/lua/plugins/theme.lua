7e6b440afb30b9bad9f44177697902341e1e8337
Piotr Chmolowski <piotr@chmolowski.pl>
2026-05-29 09:02:27 +0200

    try kanagawa theme

--- dotfiles/.config/nvim/lua/plugins/theme.lua
+++ dotfiles/.config/nvim/lua/plugins/theme.lua
@@ -1,9 +1,39 @@
 return {
+  -- {
+  --   "deparr/tairiki.nvim",
+  --   priority = 1000,
+  --   config = function()
+  --     vim.cmd.colorscheme("tairiki")
+  --   end,
+  -- },
+  -- {
+  --   "folke/tokyonight.nvim",
+  --   priority = 1000,
+  --   config = function()
+  --     vim.cmd.colorscheme("tokyonight-night")
+  --   end,
+  -- },
   {
-    "deparr/tairiki.nvim",
+    "rebelot/kanagawa.nvim",
     priority = 1000,
     config = function()
-      vim.cmd.colorscheme("tairiki")
+      require("kanagawa").setup({
+        theme = "wave",
+        colors = {
+          -- palette = {},
+          theme = {
+            wave = {},
+            lotus = {},
+            dragon = {},
+            all = {
+              ui = {
+                bg_gutter = "none"
+              }
+            }
+          },
+        },
+      })
+      vim.cmd.colorscheme("kanagawa")
     end,
   },
 }
